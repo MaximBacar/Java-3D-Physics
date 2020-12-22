a946bee974cd1c1e128f7eae3911c2bc1de2e224
Maxim Bacar <maximbacar@hotmail.ca>
2020-12-22 13:00:04 -0500

    Vector 3D class

---
+++ PIF/src/pif/physics/Vector3D.java
@@ -0,0 +1,26 @@
+package pif.physics;
+
+public class Vector3D {
+	
+	private double x;
+	private double y;
+	private double z;
+	
+	public Vector3D () {
+		
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+		
+	}
+	
+	public Vector3D (double x, double y, double z) {
+		
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		
+	}
+	
+
+}
